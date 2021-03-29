e320491e1bb66de5a28c5e28d0845025d24c7efc
Matthew Kukulski <Matthew.Kukulski@du.edu>
2021-03-28 18:40:40 -0600

    remove pw import from flask

--- flask_app.py
+++ flask_app.py
@@ -5,7 +5,6 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-from config import pw
 
 
 ################################################
